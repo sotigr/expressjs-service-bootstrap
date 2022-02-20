3cfc3d5f35f6ccd793d14a96a41f018c70aecca5
sotig <sotiris.papaioannou@hotmail.com>
2022-02-21 00:16:39 +0200

    rabbitmq

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const allowedHostsMiddleware = require("./middleware/allowed-hosts")
+const rabbitmqMiddleware = require("./middleware/rabbitmq")
 const bodyParser = require('body-parser')
+const rabbitmq = require("./middleware/rabbitmq")
 
 const defaultConfig = {
     allowedHosts: [],
     statusUrl: "/status",
+    rabbitmq: null
 }
 
 
 module.exports = (app, config) => {
-    config = config || defaultConfig;
+    if (!config) config = {};
+    config = {
+        ...defaultConfig,
+        ...config
+    }
 
-    app.get(config.statusUrl || defaultConfig.statusUrl, (req, res) => {
+    app.get(config.statusUrl, (req, res) => {
         res.end("ok")
     })
-
+   
     app.use(bodyParser.json())
 
     app.use(allowedHostsMiddleware(config.allowedHosts))
+    
+    let rabbitmqMw
+    if (config.rabbitmq) {
+        rabbitmqMw = await rabbitmqMiddleware(config.rabbitmq)
+        app.use(rabbitmqMw.middleware)
+    }
+
+    return {
+        rabbitmq: rabbitmqMw.rabbitmq
+    }
 }
